38f29c53c56a60d2345f8edb12c7f98eb059ac6a
hs8945 <417726753@qq.com>
2023-07-24 19:19:12 +0800

    Update module.py

--- module.py
+++ module.py
@@ -56,71 +56,29 @@ def rnn_model_2(x):
     return audio_outputs
   
   
-def new_context_gating(name, input_layer):
-     with tf.variable_scope(name+'new_context_gating'):
-            
-#         vlad = input_layer.slim.dropout(vlad, keep_prob=0.5, is_training=self.is_training, scope="vlad_dropout")
+def context_gating(name, input_layer, add_batch_norm=True):
+    with tf.variable_scope(name + 'context_gating'):
 
-        vlad_dim = input_layer.get_shape().as_list()[1]
-#         vlad = slim.dropout(input_layer, keep_prob=dropout_keep_prob, is_training=True, scope="vlad_dropout1")
-        print("VLAD dimension", vlad_dim)
-        hidden1_weights_1 = tf.get_variable("hidden1_weights_1",
-                                          [vlad_dim, 2048],    #1024x2048
-                                          initializer=slim.variance_scaling_initializer())
-#                                             initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(vlad_dim)))
+        input_dim = input_layer.get_shape().as_list()[1]
 
-        activation = tf.matmul(input_layer, hidden1_weights_1) #2048
-#         print('activation dimention',activation.get_shape().as_list()[1])
-        activation = slim.batch_norm(
-            activation,
-            center=True,
-            scale=True,
-            is_training=True,
-            scope="hidden1_bn",
-            fused=False)
-        gating_weights_1 = tf.get_variable("gating_weights_1",
-#                                            [hidden1_size, hidden1_size // 8],
-#                                            [2048,2048],
-                                           [2048,256],
-                                           initializer=slim.variance_scaling_initializer())
-#                                            initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(vlad_dim)))
+        gating_weights = tf.get_variable("gating_weights", [input_dim, input_dim],
+                                         initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(input_dim)))
 
-        gates = tf.matmul(activation, gating_weights_1)     
-#         print('gates dimention',gates.get_shape().as_list()[1])
-#         gates = tf.matmul(input_layer, gating_weights_1)
+        gates = tf.matmul(input_layer, gating_weights)
 
+        if add_batch_norm:
+            gates = slim.batch_norm(gates, center=True, scale=True, is_training=True,
+                                    scope="gating_bn")
+        else:
+            gating_biases = tf.get_variable("gating_biases", [input_dim],
+                                            initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(input_dim)))
+            gates += gating_biases
 
-        gates = slim.batch_norm(
-            gates,
-            center=True,
-            scale=True,
-            is_training=True,
-            activation_fn=slim.nn.relu,
-            scope="gating_bn")
+        gates = tf.nn.sigmoid(gates)
 
-        gating_weights_2 = tf.get_variable("gating_weights_2",
-                                                                                     [256,2048],
-                                           initializer=slim.variance_scaling_initializer()
-#                                            initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(vlad_dim))
-                                           )
-        gates = tf.matmul(gates, gating_weights_2) #2048
-#         print('gates dimention',gates.get_shape().as_list()[1])
-        gates = slim.batch_norm(
-            gates,
-            center=True,
-            scale=True,
-            is_training=True,
-            activation_fn=slim.nn.relu,
-            scope="gating_bn2")
+        activation = tf.multiply(input_layer, gates)
 
-        gates = tf.sigmoid(gates)
-#         tf.summary.histogram("final_gates", gates)
-
-        activation = tf.multiply(activation, gates)
-#         activation = tf.multiply(input_layer,gates)
-        l2_penalty = 1e-8
-        
-        return activation
+    return activation
 
     
   # NetVLAD 2016 CVPR NetVLAD: CNN architecture for weakly supervised place recognition
@@ -181,3 +139,161 @@ def NetVLAD(name,reshaped_input, feature_size=512, max_samples=6, cluster_size=3
         vlad = tf.matmul(vlad, hidden1_weights)
 
     return vlad    
+
+# NetVLAD的改进版
+# 来自论文：NeXtVLAD: An Efficient Neural Network to Aggregate Frame-level Features for Large-scale Video Classification ECCV2018 2nd Workshop
+# 增加了一个attention
+def NeXtVLAD(name, input_tensor, feature_size=100, max_samples=30, cluster_size=32, output_dim=256, group=128,
+             add_batch_norm=True):
+    with tf.variable_scope(name + 'forward'):
+        # 第1步：升维 N -> 2N
+        expansion = 2
+        #         input_tensor = slim.fully_connected(input_tensor, expansion * feature_size, activation_fn=None, weights_initializer=slim.variance_scaling_initializer())
+        input_tensor = slim.fully_connected(input_tensor, expansion * feature_size, activation_fn=tf.nn.leaky_relu,
+                                            weights_initializer=slim.variance_scaling_initializer())
+
+        ################################################################################################
+        # 第2步： attention 注意力函数
+        groups = group
+
+        attention = slim.fully_connected(input_tensor, groups, activation_fn=tf.nn.sigmoid,
+                                         weights_initializer=slim.variance_scaling_initializer())
+
+        attention = tf.reshape(attention, [-1, max_samples * groups, 1])
+        print('NeXtVLAD attention shape', attention.shape)
+        ################################################################################################
+        # softmax 软分配
+        new_feature_size = expansion * feature_size // groups
+        #         new_feature_size = expansion * feature_size/groups
+
+        cluster_weights = tf.get_variable("cluster_weights",
+                                          [expansion * feature_size, groups * cluster_size],
+                                          initializer=slim.variance_scaling_initializer()
+                                          )
+
+        reshaped_input = tf.reshape(input_tensor, [-1, expansion * feature_size])
+        activation = tf.matmul(reshaped_input, cluster_weights)
+
+        activation = slim.batch_norm(activation, center=True, scale=True, is_training=True, scope="cluster_bn1")
+
+        activation = tf.reshape(activation, [-1, max_samples * groups, cluster_size])
+        activation = tf.nn.softmax(activation, dim=-1)
+        ###################################################################################################
+        # 公式（5）的前半部分 ag * agk
+        activation = tf.multiply(activation, attention)
+
+        ###################################################################################################
+        # 减法
+
+        a_sum = tf.reduce_sum(activation, 1, keep_dims=True)
+
+        # 聚类中心，也是学出来的
+        cluster_weights2 = tf.get_variable("cluster_weights2",
+                                           [1, new_feature_size, cluster_size],
+                                           initializer=slim.variance_scaling_initializer()
+                                           )
+        a = tf.multiply(a_sum, cluster_weights2)
+        ###################################################################################################
+        # 被减法
+        activation = tf.transpose(activation, perm=[0, 2, 1])
+
+        reshaped_input = tf.reshape(input_tensor, [-1, max_samples * groups, new_feature_size])
+
+        vlad = tf.matmul(activation, reshaped_input)
+        vlad = tf.transpose(vlad, perm=[0, 2, 1])
+        ##################################################################################################
+        # 被减法 - 减法        公式（6）完成
+        vlad = tf.subtract(vlad, a)
+
+        ###################################################################################################
+        # 归一化
+        vlad = tf.nn.l2_normalize(vlad, 1)
+        vlad = tf.reshape(vlad, [-1, cluster_size * new_feature_size])
+        vlad = tf.nn.l2_normalize(vlad, 1)  ##保留尾部归一化
+        vlad = slim.batch_norm(vlad, center=True, scale=True, is_training=True, scope="vlad_bn")
+
+        # 主要为了是输出相应维度的向量
+        hidden1_weights = tf.get_variable("hidden1_weights1", [cluster_size * new_feature_size, output_dim],
+                                          initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(cluster_size))
+                                          )
+
+        vlad = tf.matmul(vlad, hidden1_weights)  # (?, 256)
+        return vlad
+
+    # NeXtVLAD的改进版
+
+
+# herad +ReLU,  tail+L2
+def NNeXtVLAD(name, inputs, feature_size=512, max_samples=6, cluster_size=32, output_dim=256, group=128,
+              add_batch_norm=True):
+    with tf.variable_scope(name + 'forward'):
+        expansion = 2
+        print('inputs before dim', inputs.shape)  
+        # 按行shuffle
+        #         inputs = tf.transpose(inputs,[1,0,2])   
+        #         inputs = tf.random_shuffle(inputs)
+        #         inputs = tf.transpose(inputs,[1,0,2])
+        #         inputs = tf.random_shuffle(inputs)  
+        inputs = slim.fully_connected(inputs, expansion * feature_size, activation_fn=tf.nn.relu,
+                                      weights_initializer=slim.variance_scaling_initializer())
+        #         inputs     = slim.fully_connected(inputs, expansion * feature_size, activation_fn=tf.nn.leaky_relu, weights_initializer=slim.variance_scaling_initializer())
+        print('inputs after dim', inputs.shape)  # 11*8192
+        ################################################################################################
+        # 2： attention
+        groups = group  # 16
+        result = slim.fully_connected(inputs, groups, activation_fn=tf.nn.sigmoid,
+                                      weights_initializer=slim.variance_scaling_initializer())
+        print('results dims', result.shape)  # 11*16
+        attention = tf.reshape(result, [-1, max_samples * groups, 1])  
+        print('attention dims', attention.shape)
+        ################################################################################################
+        reshaped_input = tf.reshape(inputs, [-1, expansion * feature_size])  # 11*8192
+        new_feature_size = expansion * feature_size // groups  
+        cluster_weights = tf.get_variable("cluster_weights",
+                                          [expansion * feature_size, groups * cluster_size],
+                                          initializer=slim.variance_scaling_initializer()
+                                          )
+        print('cluster_weights shape', cluster_weights.shape)  # 8192*512
+        activation = tf.matmul(reshaped_input, cluster_weights)
+        print('activation shape before BN', activation.shape)  # 11*512
+        activation = slim.batch_norm(activation, center=True, scale=True,
+                                     is_training=True, scope="cluster_bn")
+
+        activation = tf.reshape(activation, [-1, max_samples * groups, cluster_size])
+        print('activation shape after BN', activation.shape)  # 176*32
+        activation = tf.nn.softmax(activation, dim=-1)
+        ###################################################################################################
+        activation = tf.multiply(activation, attention)  # cluster center
+        print('activation shape multiply attention', activation.shape)
+
+        ###################################################################################################
+        a_sum = tf.reduce_sum(activation, 1, keep_dims=True)
+        cluster_weights2 = tf.get_variable("cluster_weights2",
+                                           [1, new_feature_size, cluster_size],
+                                           initializer=slim.variance_scaling_initializer()
+                                           )
+        a = tf.multiply(a_sum, cluster_weights2)
+        ###################################################################################################
+        activation = tf.transpose(activation, perm=[0, 2, 1])  # 32*176
+
+        reshaped_input = tf.reshape(inputs, [-1, max_samples * groups, new_feature_size])
+
+        vlad = tf.matmul(activation, reshaped_input)  # 32*512
+        vlad = tf.transpose(vlad, perm=[0, 2, 1])  # 512*32
+        ###################################################################################################
+        vlad = tf.subtract(vlad, a)
+
+        ###################################################################################################
+        vlad = tf.nn.l2_normalize(vlad, 1)  # intra-normalization
+        vlad = tf.reshape(vlad, [-1, cluster_size * new_feature_size])  # 16384
+        vlad = tf.nn.l2_normalize(vlad, 1)  # L2-normalization
+
+        vlad = slim.batch_norm(vlad, center=True, scale=True, is_training=True, scope="vlad_bn")
+        hidden1_weights = tf.get_variable("hidden1_weights",
+                                          [cluster_size * new_feature_size, output_dim],
+                                          initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(cluster_size))
+                                          )
+
+        vlad = tf.matmul(vlad, hidden1_weights)
+
+        return vlad
